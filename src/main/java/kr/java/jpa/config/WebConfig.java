4732f899d7a12b08c4a02fea49ad4de369c3ac86
kimjava911 <aibackend4th@gmail.com>
2025-11-25 10:24:55 +0900

    Initial commit

---
+++ src/main/java/kr/java/jpa/config/WebConfig.java
@@ -0,0 +1,17 @@
+package kr.java.jpa.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan("kr.java.jpa") // 최상단 패키지
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/views/", ".jsp");
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>메인 페이지</title>
+</head>
+<body>
+    <p>메인 페이지</p>
+</body>
+</html>
